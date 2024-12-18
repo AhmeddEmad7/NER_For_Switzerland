721a301a025270f1cbcc43096d11faf90a72f26e
MohamedAlaaAli <muhammed.157@gmail.com>
2024-12-18 12:16:31 +0200

    finish xlm-roberta

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from transformers import AutoTokenizer, AutoConfig, DataCollatorForTokenClassifi
 from transformers.trainer import TrainingArguments, Trainer
 import numpy as np
 import evals
+from collections import defaultdict, Counter
+from datasets import concatenate_datasets
+from datasets import DatasetDict
+import pandas as pd
+
+
+
 
 panx_ch = prepare_data.get_data()
 tags = prepare_data.tags
@@ -23,6 +30,16 @@ data_collator = DataCollatorForTokenClassification(xlmr_tokenizer)
 
 def model_init():
     return (XlmRModel.XLMRobertaForTokenClassification.from_pretrained("xlm-roberta-base", config=xlmr_config).to(device))
+
+def evaluate_lang_performance(lang, trainer):
+    panx_ds = prepare_data.encode_panx_dataset(panx_ch[lang])
+    return evals.get_f1_score(trainer, panx_ds["test"]), 
+
+def concatenate_splits(corpora):
+    multi_corpus = DatasetDict()
+    for split in corpora[0].keys():
+        multi_corpus[split] = concatenate_datasets([corpus[split] for corpus in corpora]).shuffle(seed=42)
+    return multi_corpus
 
 def align_predictions(predictions, label_ids):
     """
@@ -60,27 +77,73 @@ def align_predictions(predictions, label_ids):
         
     return preds_list, labels_list
 
-def main(zero_shot_flag=True, multi_flag=True, n_epoch=3, batch_size=24,):
+def main(multi_flag=True, n_epoch=3, batch_size=24,):
 
-    if zero_shot_flag:
-        panx_de_encoded = prepare_data.encode_panx_dataset(panx_ch["de"])
-        num_epochs = 3
-        batch_size = 24
-        logging_steps = len(panx_de_encoded["train"]) // batch_size
-        model_name = f"xlm-roberta-base-finetuned-panx-de"
-        training_args = TrainingArguments(
-            output_dir=model_name, log_level="error", num_train_epochs=num_epochs,
-            per_device_train_batch_size=batch_size,
-            per_device_eval_batch_size=batch_size, eval_strategy="epoch",
-            save_steps=1e6, weight_decay=0.01, disable_tqdm=False,
-            logging_steps=logging_steps, push_to_hub=False)
+##########################################Zero Shot langugage Transfer####################################################
+    panx_de_encoded = prepare_data.encode_panx_dataset(panx_ch["de"])
+    num_epochs = 3
+    batch_size = 24
+    logging_steps = len(panx_de_encoded["train"]) // batch_size
+    model_name = f"xlm-roberta-base-finetuned-panx-de"
+
+    training_args = TrainingArguments(
+        output_dir=model_name, log_level="error", num_train_epochs=num_epochs,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size, eval_strategy="epoch",
+        save_steps=1e6, weight_decay=0.01, disable_tqdm=False,
+        logging_steps=logging_steps, push_to_hub=False)
+    
+    trainer = Trainer(model_init=model_init, args=training_args,
+        data_collator=data_collator, compute_metrics=evals.compute_metrics,
+        train_dataset=panx_de_encoded["train"],
+        eval_dataset=panx_de_encoded["validation"],
+        tokenizer=xlmr_tokenizer)
         
+    trainer.train() 
+    f1_scores = defaultdict(dict)
+    f1_scores["de"]["de"] = evals.get_f1_score(trainer, panx_de_encoded["test"])
+    print(f"F1-score of [de] model on [de] dataset: {f1_scores['de']['de']:.3f}")
+    f1_scores["de"]["fr"] = evaluate_lang_performance("fr", trainer)
+    print(f"F1-score of [de] model on [fr] dataset: {f1_scores['de']['fr']:.3f}")
+
+###################################################################Mutli Linguagl##############################################
+    if multi_flag:
+        panx_fr_encoded = prepare_data.encode_panx_dataset(panx_ch["fr"])
+        panx_de_fr_encoded = concatenate_splits([panx_de_encoded, panx_fr_encoded])
+        training_args.logging_steps = len(panx_de_fr_encoded["train"]) // batch_size
+        training_args.push_to_hub = False
+        training_args.output_dir = "xlm-roberta-base-finetuned-panx-de-fr"
         trainer = Trainer(model_init=model_init, args=training_args,
-            data_collator=data_collator, compute_metrics=evals.compute_metrics,
-            train_dataset=panx_de_encoded["train"],
-            eval_dataset=panx_de_encoded["validation"],
-            tokenizer=xlmr_tokenizer)
-        
-        trainer.train() 
+        data_collator=data_collator, compute_metrics=evals.compute_metrics,
+        tokenizer=xlmr_tokenizer, train_dataset=panx_de_fr_encoded["train"],
+        eval_dataset=panx_de_fr_encoded["validation"])
+        trainer.train()
 
+        langs = ["de", "fr", "it", "en"]
+        for lang in langs:
+            f1 = evaluate_lang_performance(lang, trainer)
+            print(f"F1-score of [de-fr] model on [{lang}] dataset: {f1:.3f}")
+        
+        corpora = [panx_de_encoded]
+        for lang in langs[1:]:
+            ds_encoded = prepare_data.encode_panx_dataset(panx_ch[lang])
+            corpora.append(ds_encoded)
+
+        corpora_encoded = concatenate_splits(corpora)
+        training_args.logging_steps = len(corpora_encoded["train"]) // batch_size
+        training_args.output_dir = "xlm-roberta-base-finetuned-panx-all"
+        trainer = Trainer(model_init=model_init, args=training_args,
+        data_collator=data_collator, compute_metrics=evals.compute_metrics,
+        tokenizer=xlmr_tokenizer, train_dataset=corpora_encoded["train"],
+        eval_dataset=corpora_encoded["validation"])
+        trainer.train()
+
+        for idx, lang in enumerate(langs):
+            f1_scores["all"][lang] = evals.get_f1_score(trainer, corpora[idx]["test"])
+            scores_data = {"de": f1_scores["de"],
+            "all": f1_scores["all"]}
+            f1_scores_df = pd.DataFrame(scores_data).T.round(4)
+            f1_scores_df.rename_axis(index="Fine-tune on", columns="Evaluated on",
+            inplace=True)
+            f1_scores_df
 
